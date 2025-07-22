613f1096eb961a2ffdad2217a5ea7182c0d994f6
LYJ0304 <omlmg0304@naver.com>
2025-07-22 15:22:01 +0900

    feat: JpaRepository 기반 BoardRepository 생성

--- src/main/java/com/example/board/repository/BoardRepository.java
+++ src/main/java/com/example/board/repository/BoardRepository.java
@@ -1,4 +1,8 @@
 package com.example.board.repository;
 
-public interface BoardRepository {
+import com.example.board.entity.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+
 }
