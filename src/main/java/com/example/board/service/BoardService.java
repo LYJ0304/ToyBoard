b2b3e0727f0651ff90198f48f0e135c3e91c9b3f
LYJ0304 <omlmg0304@naver.com>
2025-07-23 11:54:22 +0900

    feat: BoardEntity 목록을 BoardDTO 목록으로 변환하는 findAll 메서드 구현

--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -1,10 +1,14 @@
 package com.example.board.service;
 
 import com.example.board.dto.BoardDTO;
+import com.example.board.entity.BaseEntity;
 import com.example.board.entity.BoardEntity;
 import com.example.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // DTO -> Entity
 // Entity -> DTO
@@ -17,5 +21,15 @@ public class BoardService {
     public void save(BoardDTO boardDTO) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(boardEntity);
+    }
+
+    public List<BoardDTO> findAll() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        BaseEntity baseEntity = new BaseEntity();
+        for (BoardEntity boardEntity: boardEntityList) {
+            boardDTOList.add(BoardDTO.toboardDTO(boardEntity, baseEntity));
+        }
+        return boardDTOList;
     }
 }
